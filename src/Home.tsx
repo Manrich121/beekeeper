a6374e98d0d6a825662f5f36878da4009c1d1125
Manrich <manrich@journeyapps.com>
2020-02-18 08:59:33 +0200

    Initial test using google form

--- src/Home.tsx
+++ src/Home.tsx
@@ -77,7 +77,16 @@ export default function Home(props: { manifest: CalendarManifest | null }) {
                 setProvince(selected);
               }}
             />
-            <StayInTouchForm province={province} />
+            <iframe
+              src="https://docs.google.com/forms/d/e/1FAIpQLSfTUaSMZXEhqwn5WftAXgNS278p97sTyclMrJ8Tb8bqQjMs3Q/viewform?embedded=true"
+              width={theme.spacing(60)}
+              height={600}
+              frameBorder={0}
+              marginHeight={0}
+              marginWidth={0}>
+              Loading…
+            </iframe>
+            {/*<StayInTouchForm province={province}*/}
           </>
         }
       />
