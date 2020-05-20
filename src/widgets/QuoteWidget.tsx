65a6ade7914fb9ad2e1e9fbb7b4f63e3883eb952
Manrich <manrich@journeyapps.com>
2020-05-20 17:25:34 +0200

    Turn off ts strict and remove unused import

--- src/widgets/QuoteWidget.tsx
+++ src/widgets/QuoteWidget.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, useTheme } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
